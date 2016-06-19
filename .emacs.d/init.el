da1c1cd6891780d2a0adb59bf55c0951330f5b94
Yoshiyuki Toda <ytoda@Yoshiyuki-no-MacBook-Pro.local>
2016-06-19 11:24:39 +0900

    Initial commit

---
+++ .emacs.d/init.el
@@ -0,0 +1,33 @@
+;; ロードパスの追加
+(setq load-path (append
+		 '("~/.emacs.d/packages")
+		 load-path))
+
+;; 言語設定
+(set-locale-environment nil)
+
+;; スタートアップ非表示
+(setq inhibit-startup-screen t)
+
+;; scratchの初期メッセージ消去
+(setq initial-scratch-message "")
+
+;; ツールバー非表示
+(tool-bar-mode -1)
+
+;; スクロールバー非表示
+(set-scroll-bar-mode nil)
+
+;; タイトルバーにフルパス表示
+;(setq frame-title-format
+;      (format "%%f - Emacs@%s" (system-name)))
+
+;; 行番号表示
+(global-linum-mode t)
+
+;; タブをスペースに
+(setq-default tab-width 4 indent-tabs-mode nil)
+
+;; バックスペース
+(keyboard-translate ?\C-h ?\C-?)
+
